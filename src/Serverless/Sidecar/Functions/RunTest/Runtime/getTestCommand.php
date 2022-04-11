98a943ee8123af5344b66b4f5a34824b1285b651
Luke Downing <lukeraymonddowning@gmail.com>
2022-04-11 15:22:23 +0100

    More work

--- src/Serverless/Sidecar/Functions/RunTest/Runtime/getTestCommand.php
+++ src/Serverless/Sidecar/Functions/RunTest/Runtime/getTestCommand.php
@@ -6,14 +6,20 @@ declare(strict_types=1);
 $payload = json_decode($_SERVER['argv'][1], true);
 
 $localCwd = $payload['localCwd'];
+$tempFile = $payload['tempFile'];
 $testCommand = $payload['testCommand'];
 
 /**
  * We need to replace all reference to the user's local project with
  * the location of the project on lambda.
  */
-$testCommand = array_map(function (string $commandPart) use ($localCwd) {
-    return str_replace($localCwd, '/tmp/project', $commandPart);
+$testCommand = array_map(function (string $commandPart) use ($localCwd, $tempFile) {
+    $result = str_replace($localCwd, '/tmp/project', $commandPart);
+    return str_replace(
+        substr($tempFile, 0, strrpos($tempFile, '/') + 1),
+        '/tmp',
+        $result
+    );
 }, $testCommand);
 
 echo join(' ', $testCommand);
