b170817373cf95b5e8b91ed582746b3d38571502
Bastien Philippe <bastien.philippe@soyhuce.fr>
2022-04-29 08:38:30 +0200

    Add check on ParallelRunner existence

--- src/Support/Environment.php
+++ src/Support/Environment.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Pest\Parallel\Support;
 
 use Illuminate\Foundation\Application;
+use Illuminate\Testing\ParallelRunner;
 use Orchestra\Testbench\TestCase;
 
 /**
@@ -14,6 +15,8 @@ final class Environment
 {
     public static function isALaravelApplication(): bool
     {
-        return class_exists(Application::class) && !class_exists(TestCase::class);
+        return class_exists(Application::class)
+            && class_exists(ParallelRunner::class)
+            && !class_exists(TestCase::class);
     }
 }
